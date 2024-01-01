08814e614c33fddd4df9034eb98ad2509ffa0978
Abhinav9164 <abhinav6981@gmail.com>
2024-01-01 16:08:55 +0000

    validation added in the forms

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import "../utils/validate"
 import Header from './Header'
+import { checkValidateData } from '../utils/validate';
 
 const Login = () => {
 
    const [SignInForm,setSignForm] = useState(false);
-
+   const [errorMessage,setErrorMassage] = useState(null);
+   
+   const email = useRef(null);
+   const password = useRef(null);
+   
+   const handleButtonClick = () => {
+ 
+    const message = checkValidateData(email.current.value,password.current.value);
+     
+    setErrorMassage(message);
+    
+   }
 
    const toggleSignIn = () => {
       setSignForm(!SignInForm);
@@ -18,7 +31,9 @@ const Login = () => {
           alt="bodyimage"
           />
       </div>
-       <form className='absolute w-3/12 h-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+       <form 
+       onSubmit={(e) => e.preventDefault()}
+       className='absolute w-3/12 h-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
         <h1 className='font-bold text-2xl '>
             
             {!SignInForm ? "Sign Up" : "Sign In"}
@@ -31,16 +46,22 @@ const Login = () => {
          placeholder='Email Address' />   } 
 
         <input 
+        ref={email}
         className='p-2 m-4 w-full bg-gray-700'
         type='text'
          placeholder='Email Address' />
 
         <input
+        ref={password}
         className='p-2 m-4 w-full bg-gray-700' 
         type='text' 
         placeholder='Password' />
+        
+        <p className='text-red-700 font-bold'>{errorMessage}</p>
 
-        <button className='text-sm p-4 m-4 h-12 bg-red-700 w-full rounded-lg'>
+        <button 
+        onClick={handleButtonClick}
+        className='text-sm p-4 m-4 h-12 bg-red-700 w-full rounded-lg'>
         {!SignInForm ? "Sign In" : "Sign Out"}
         </button>
 
