1b50e063f3abf7f466d11860bb6888ec623be0df
Abhinav9164 <abhinav6981@gmail.com>
2024-01-09 23:17:11 +0000

    Play and info btn hidden in small devices

--- src/Components/VedioTitle.js
+++ src/Components/VedioTitle.js
@@ -6,7 +6,7 @@ const VedioTitle = ( {title,overview} ) => {
         <h1 className='text-xl md:text-3xl font-bold mt-40 md:mt-0 ml-15'>{title}</h1>
         <p className='hidden md:inline-block pt-6 py-6 text-sm w-1/4 font-sans'>{overview}</p>
         <div className='pt-2'>
-            <button className='bg-white text-black p-2 px-6 rounded-lg'>Play</button>
+            <button className=' hidden md:inline-block bg-white text-black p-2 px-6 rounded-lg'>Play</button>
             <button className='bg-white text-black hidden md:inline-block p-2 px-6 mx-2 rounded-lg'>Play Info.</button>
         </div>
 
